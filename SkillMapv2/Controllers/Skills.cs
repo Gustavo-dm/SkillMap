34f14f4fd5633819b2c679ec8da61549f36bf2b0
gustavo-dm <gustavothemoraes@gmail.com>
2022-04-12 16:13:59 -0300

    first commit

---
+++ SkillMapv2/Controllers/Skills.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Skill_Map.Controllers
+{ 
+    public class Skills : Controller { 
+    /*
+    {
+        private readonly Repos.EmployeeRepos _employeeRepo;
+        public Skills(Repos.EmployeeRepos employeeRepo)
+        {
+
+        }
+        */
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+        public IActionResult Apagar()
+        {
+            return View();
+        }
+    }
+}
---
+++ SkillMapv2/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Skill_Map.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Skill { get; set; }
+
+        public int Nível { get; set; }
+
+        public bool WantToLearn { get; set; }
+    }
+}
